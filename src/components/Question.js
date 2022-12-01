8a3836a7878115102f7a5ac0cba665f6ea8f326b
Sienna <remicknsisi@gmail.com>
2022-12-01 09:34:39 -0500

    commit tree e12d9053cfb2e3d0a048327e57852d57867e7c4d

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+
+useEffect(() => {
+  const timerId = setTimeout(()=>setTimeRemaining(timeRemaining -1), 1000)
+
+  if (timeRemaining === 0){
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
+
+  return function cleanup(){
+    clearTimeout(timerId)
+  }
+})
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
@@ -24,7 +36,7 @@ function Question({ question, onAnswered }) {
           </button>
         );
       })}
-      <h5>{timeRemaining} seconds remaining</h5>
+      <h5 >{timeRemaining} seconds remaining</h5>
     </>
   );
 }
